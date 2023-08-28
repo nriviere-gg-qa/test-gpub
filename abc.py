ec29215e20317aea918c2d6f9b770b1c93fecd42
nriviere-gg-qa <nriviere-gg-qa@proton.me>
2023-08-28 11:16:56 +0200

    Update abc.py

--- abc.py
+++ abc.py
@@ -4,3 +4,18 @@ password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09'
 password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
 password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
 password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
+
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
+
+
+
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
+
+
+
+
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
+
+
+
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
