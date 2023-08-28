96ec50c8cf27551821b9a22832b76a01b365fdc9
nriviere-gg-qa <nriviere-gg-qa@proton.me>
2023-08-28 11:01:55 +0200

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,6 @@
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
+password = 'lknrklfj2309ur2309fjklsfjkldj2r3i32r09' 
